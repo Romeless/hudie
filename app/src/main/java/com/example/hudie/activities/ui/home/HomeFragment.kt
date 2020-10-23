3908415345e405a6c96b44f43c37deda37d39682
Rama Lesmana <ramalesmana.1999@gmail.com>
2020-10-23 21:16:18 +0700

    Shop Implementation

--- app/src/main/java/com/example/hudie/activities/ui/home/HomeFragment.kt
+++ app/src/main/java/com/example/hudie/activities/ui/home/HomeFragment.kt
@@ -1,6 +1,7 @@
 package com.example.hudie.activities.ui.home
 
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -16,13 +17,13 @@ import com.example.hudie.activities.ui.dashboard.DashboardFragment
 import com.example.hudie.adapters.ShopCardAdapter
 import com.example.hudie.api.RetrofitClient
 import com.example.hudie.models.DesignResponse
+import kotlinx.android.synthetic.main.fragment_dashboard.*
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
 
 class HomeFragment : Fragment() {
 
-    private lateinit var homeViewModel: HomeViewModel
 
     companion object {
         fun newInstance(): HomeFragment = HomeFragment();
@@ -33,17 +34,25 @@ class HomeFragment : Fragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        homeViewModel =
-            ViewModelProviders.of(this).get(HomeViewModel::class.java)
+        Log.i("HOME", "onCreateView")
+
         val view = inflater.inflate(R.layout.fragment_home, container, false);
 
         val mRecyclerView = view.findViewById<RecyclerView>(R.id.recycler_view)
+        mRecyclerView.setHasFixedSize(true)
+
+        val layoutManager = LinearLayoutManager(this.context)
+        layoutManager.orientation = LinearLayoutManager.VERTICAL
+
+        mRecyclerView.layoutManager = layoutManager
 
         RetrofitClient.instance.getDesigns().enqueue(object: Callback<ArrayList<DesignResponse>> {
             override fun onResponse(
                 call: Call<ArrayList<DesignResponse>>,
                 response: Response<ArrayList<DesignResponse>>
             ) {
+                Log.i("HOME", "onRespond")
+
                 val designList = response.body()
                 val mRecyclerAdapter = designList?.let { ShopCardAdapter(it) }
 
@@ -52,6 +61,7 @@ class HomeFragment : Fragment() {
             }
 
             override fun onFailure(call: Call<ArrayList<DesignResponse>>, t: Throwable) {
+                Log.i("HOME", "onFailure")
                 TODO("Not yet implemented")
             }
 
--- app/src/main/java/com/example/hudie/adapters/ShopCardAdapter.kt
+++ app/src/main/java/com/example/hudie/adapters/ShopCardAdapter.kt
@@ -16,23 +16,22 @@ class ShopCardAdapter(private val designList: ArrayList<DesignResponse>) : Recyc
     }
 
     inner class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
-        var itemImage: ImageView? = itemView.findViewById<ImageView?>(R.id.design_image)
-        var itemTitle: TextView? = itemView.findViewById<TextView?>(R.id.design_title)
-        var itemDesigner: TextView? = itemView.findViewById<TextView?>(R.id.design_creator)
-        var itemDetail: TextView? = itemView.findViewById<TextView?>(R.id.design_detail)
+        // var itemImage: ImageView = itemView.findViewById<ImageView>(R.id.design_image)
+        var itemTitle: TextView = itemView.findViewById<TextView>(R.id.design_title)
+        var itemDesigner: TextView = itemView.findViewById<TextView>(R.id.design_creator)
+        var itemDetail: TextView = itemView.findViewById<TextView>(R.id.design_detail)
     }
 
     override fun onCreateViewHolder(viewGroup: ViewGroup, i: Int): ViewHolder {
-        val view = LayoutInflater.from(viewGroup?.context)
+        val view = LayoutInflater.from(viewGroup.context)
             .inflate(R.layout.cardview_shop_design, viewGroup, false)
         return ViewHolder(view)
     }
 
     override fun onBindViewHolder(viewHolder: ViewHolder, i: Int) {
-        // viewHolder?.itemImage
-        viewHolder?.itemTitle?.text = designList[i].design_name
-        viewHolder?.itemDesigner?.text = designList[i].username
-        viewHolder?.itemDetail?.text = designList[i].details
+        viewHolder.itemTitle.text = designList[i].design_name
+        viewHolder.itemDesigner.text = designList[i].username
+        viewHolder.itemDetail.text = designList[i].details
     }
 
     override fun getItemCount(): Int = designList.size
